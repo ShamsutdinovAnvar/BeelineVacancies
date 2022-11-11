25377295ab27bd9fa3aa49891062fd1affabbce2
ShamsutdinovAnvar <a.shamsutdinov2021@gmail.com>
2022-11-11 22:33:20 +0500

    first commit

---
+++ src/test/java/com/beeline/tests/MainPageTests.java
@@ -0,0 +1,85 @@
+package com.beeline.tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainPageTests extends TestBase {
+    @Test
+    @DisplayName("Checking the main page banner")
+    void checkBannerTest() {
+        step("Открываем сайт https://www.beeline.uz/ru/", () -> {
+            open("https://www.beeline.uz/ru/");
+        });
+
+        step("check banner", () -> {
+            $("#ms-main-banners-banner1").shouldBe(visible);
+        });
+    }
+
+    @Test
+    @DisplayName("Login modal form should appear on main page")
+    void generatedTest() {
+        step("Open url 'https://www.beeline.uz/ru/'", () ->
+                open("https://www.beeline.uz/ru"));
+
+        step("Click on Profile button", () ->
+                $(".logged-out").click());
+
+        step("Login modal should be visible", () ->
+                $("#pills-register-tab-header").shouldBe(visible));
+    }
+
+    @Test
+    @DisplayName("Page title should have header text")
+    void titleTest() {
+        step("Open url 'https://www.beeline.uz/ru/'", () ->
+                open("https://www.beeline.uz/ru/"));
+
+        step("Page title should have text 'Официальный сайт сотового оператора Beeline - Узбекистан.'", () -> {
+            String expectedTitle = "Официальный сайт сотового оператора Beeline - Узбекистан.";
+            String actualTitle = title();
+
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
+    }
+
+    @Test
+    @DisplayName("Checking the Tariff Plans section")
+    void tariffPlansTest() {
+        step("Открываем сайт https://www.beeline.uz/ru/", () -> {
+            open("https://www.beeline.uz/ru/");
+        });
+
+        step("Заходим в раздел 'Тарифные планы'", () -> {
+            $$("span[class='label-nav notoSa main__menu-item']").first().click();
+        });
+
+        step("Проверка отображения страницы", () -> {
+            $(".notoSa.text-center.h2_space").shouldHave(text("Тарифные планы"));
+        });
+    }
+
+    @Test
+    @DisplayName("Checking the Internet packages section")
+    void internetPackagesTest() {
+        step("Открываем сайт https://www.beeline.uz/ru/", () -> {
+            open("https://www.beeline.uz/ru/");
+        });
+
+        step("Заходим в раздел 'Интернет пакеты'", () -> {
+            $("a[href='https://beeline.uz/ru/products/internet-paketi?utm_source=site&utm_medium=cust_exp&utm_campaign=paketi_ru']").click();
+        });
+
+        step("Проверка отображения страницы", () -> {
+            $("div[id='tarif-list1'] h2[class='notoSa text-center h2_space']").shouldHave(text("Однодневные интернет-пакеты"));
+        });
+    }
+
+
+}
---
+++ src/test/java/com/beeline/tests/TestBase.java
@@ -0,0 +1,44 @@
+package com.beeline.tests;
+
+import com.beeline.config.Project;
+import com.beeline.helpers.AllureAttachments;
+import com.beeline.helpers.DriverSettings;
+import com.beeline.helpers.DriverUtils;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        DriverSettings.configure();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    public void afterEach() {
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+//        AllureAttachments.attachNetwork(); // todo
+        AllureAttachments.addBrowserConsoleLogs();
+
+        Selenide.closeWebDriver();
+
+        if (Project.isVideoOn()) {
+            AllureAttachments.addVideo(sessionId);
+        }
+    }
+}
